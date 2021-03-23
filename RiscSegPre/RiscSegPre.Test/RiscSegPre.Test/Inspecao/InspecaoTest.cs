1f61565cecdd23a5b865d08258b70364742662a3
Douglas Leonardo Oliveira Coutinho <douglas.loc@hotmail.com>
2021-03-23 17:09:30 -0300

    Acertos no Projeto de teste

--- RiscSegPre/RiscSegPre.Test/RiscSegPre.Test/Inspecao/InspecaoTest.cs
+++ RiscSegPre/RiscSegPre.Test/RiscSegPre.Test/Inspecao/InspecaoTest.cs
@@ -1,7 +1,9 @@
 using NUnit.Framework;
+using RiscSegPre.Domain.Entities;
 
 namespace RiscSegPre.Test.Inspecao
 {
+    [TestFixture]
     public class InspecaoTest
     {
         [SetUp]
@@ -10,7 +12,21 @@ namespace RiscSegPre.Test.Inspecao
         [Test]
         public void TestInspecao()
         {
-            Assert.Pass();
+            const int resultado_esperado = 55;
+
+            var inspecao = CalcularNota.CalcularRisco(CarregarInspecao());
+            Assert.AreEqual(resultado_esperado, inspecao.nota);
+        }
+
+        private Domain.Entities.Inspecao CarregarInspecao()
+        {
+            return new Domain.Entities.Inspecao
+                (
+                    new NotaMeioProtecaoFisico(10, 10, 10, 10, 10, 10, 10, 10),
+                    new NotaMeioProtecaoHumano(80, 80, 80, 80, 80),
+                    new NotaMeioProtecaoTecnico(70, 70, 70, 70, 70, 70, 70, 70),
+                    new NotaAvaliacaoProcedimento(60, 60, 60, 60, 60)
+                );
         }
     }
 }
